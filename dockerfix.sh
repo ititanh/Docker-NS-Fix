58a1a29297651cc6a9982d8828d225cc159e2f58
ititanh <168681557+ititanh@users.noreply.github.com>
2024-05-02 00:05:19 +0300

    Update dockerfix.sh

--- dockerfix.sh
+++ dockerfix.sh
@@ -19,12 +19,12 @@ case $location in
     1)
         echo "Docker configuration will be set to Romania nameservers"
         rm daemon.json
-        wget https://raw.githubusercontent.com/ititanh/Docker-NS-Fix/master/Romania/daemon.js
+        wget https://raw.githubusercontent.com/ititanh/Docker-NS-Fix/main/Romania/daemon.json
         ;;
     2)
         echo "Docker configuration will be set to Germany nameservers"
         rm daemon.json
-        wget https://raw.githubusercontent.com/ititanh/Docker-NS-Fix/master/dockerfix.sh/Germany/daemon.js
+        wget https://raw.githubusercontent.com/ititanh/Docker-NS-Fix/main/Germany/daemon.json
         ;;
     *)
         echo "Invalid selection. Please choose either 1 or 2."
